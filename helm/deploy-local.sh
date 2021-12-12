4cf0877fb4ebf3d0ce7f027c66fadaafbf4fd72d
system <falgifaisal@gamil.com>
2021-12-12 16:44:10 +0700

    Add helm chart for test

---
+++ helm/deploy-local.sh
@@ -0,0 +1,3 @@
+docker build -t ikan-id:v.1.0 .
+docker tag ikan-id:v.1.0 localhost:5000/falgifaisal/ikan-id:v.1.0
+docker push localhost:5000/falgifaisal/ikan-id:v.1.0
---
+++ helm/restart.sh
@@ -0,0 +1,1 @@
+helm delete falgi && helm install falgi .
